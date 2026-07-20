8ef94c36250713f97b5298b9959e149b59feda7f
j <136126244+shopper12@users.noreply.github.com>
2026-07-20 15:28:29 +0900

    Activate unified news pipeline in API

--- src/telegram_news/unified_patch_v7.py
+++ src/telegram_news/unified_patch_v7.py
@@ -1,1 +1,10 @@
-# removed
+from __future__ import annotations
+
+from typing import Any
+
+from .unified_pipeline import apply_unified_pipeline
+
+
+def apply(api_module: Any) -> Any:
+    apply_unified_pipeline()
+    return api_module
