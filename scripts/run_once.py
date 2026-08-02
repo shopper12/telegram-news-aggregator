a00a9fe67bcd67559d528a7c2693196792ad6776
j <136126244+shopper12@users.noreply.github.com>
2026-08-02 14:20:03 +0900

    Install market note formatter in scheduled runs

--- scripts/run_once.py
+++ scripts/run_once.py
@@ -1,7 +1,9 @@
 from telegram_news.consistency_rules import install as install_consistency_rules
+from telegram_news.market_note_formatter import install_dispatch_hook
 from telegram_news.telegram_dispatch import main
 
 
 if __name__ == "__main__":
     install_consistency_rules()
+    install_dispatch_hook()
     main()
