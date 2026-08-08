032315ad7734b7dfae25ae376e6d493b4baa381f
j <136126244+shopper12@users.noreply.github.com>
2026-08-08 12:50:56 +0900

    Install continuous quotes in scheduled reports

--- scripts/run_once.py
+++ scripts/run_once.py
@@ -1,9 +1,11 @@
 from telegram_news.consistency_rules import install as install_consistency_rules
+from telegram_news.continuous_quote_fallback import install as install_continuous_quotes
 from telegram_news.market_note_formatter import install_dispatch_hook
 from telegram_news.telegram_dispatch import main
 
 
 if __name__ == "__main__":
+    install_continuous_quotes()
     install_consistency_rules()
     install_dispatch_hook()
     main()
