0f9e0ace6fb8d72381001e46dafb6fef99537b9b
j <136126244+shopper12@users.noreply.github.com>
2026-08-17 15:45:42 +0900

    Install official quote guard before report generation

--- scripts/run_once.py
+++ scripts/run_once.py
@@ -1,12 +1,14 @@
 from telegram_news.consistency_rules import install as install_consistency_rules
 from telegram_news.continuous_quote_fallback import install as install_continuous_quotes
 from telegram_news.market_note_formatter import install_dispatch_hook
+from telegram_news.official_quote_guard import install as install_official_quote_guard
 from telegram_news.report_integrity import install_dispatch_hook as install_report_integrity_hook
 from telegram_news.telegram_dispatch import main
 
 
 if __name__ == "__main__":
     install_continuous_quotes()
+    install_official_quote_guard()
     install_consistency_rules()
     install_dispatch_hook()
     install_report_integrity_hook()
