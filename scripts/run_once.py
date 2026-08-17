8f7ee828dac8261b7bd50a223e22047d1a79cac0
j <136126244+shopper12@users.noreply.github.com>
2026-08-17 15:41:01 +0900

    Install report integrity guard in production pipeline

--- scripts/run_once.py
+++ scripts/run_once.py
@@ -1,6 +1,7 @@
 from telegram_news.consistency_rules import install as install_consistency_rules
 from telegram_news.continuous_quote_fallback import install as install_continuous_quotes
 from telegram_news.market_note_formatter import install_dispatch_hook
+from telegram_news.report_integrity import install_dispatch_hook as install_report_integrity_hook
 from telegram_news.telegram_dispatch import main
 
 
@@ -8,4 +9,5 @@ if __name__ == "__main__":
     install_continuous_quotes()
     install_consistency_rules()
     install_dispatch_hook()
+    install_report_integrity_hook()
     main()
